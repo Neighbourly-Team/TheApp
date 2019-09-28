8ad8da67b295e5f52b8ba6b975ceb585ff48b5d4
54mb <sjburton@usc.edu>
2019-09-28 01:26:08 -0700

    Some validation added, needs reworking

--- LoginScreen/App.js
+++ LoginScreen/App.js
@@ -17,23 +17,138 @@ export default class App extends Component {
         password2: '',
         reply:[],
         userToken: '',
+            
+        badEmail: false,
+        badEmailEdu: false,
+        badFirstName: false,
+        badLastName: false,
+        badPassword: false,
+        badPasswordMatch: false,
         };
     }
     
+    
+    validData() {
+        return !(this.state.badEmail || this.state.badEmailEdu || this.state.badFirstName || this.state.badLastName || this.state.badPassword || this.state.badPasswordMatch);
+    }
+    
+    
+    validateEmail() {
+        var bad = false;
+        if (this.state.email == '') {
+            bad = true;
+        }
+        
+        if (bad) {
+            this.state.badEmail = true;
+        }
+        else {
+            this.state.badEmail = false;
+        }
+        this.forceUpdate();
+        
+        if (bad)
+            return false;
+        return true;
+    }
+    
+    validateFirstName() {
+        var bad = false;
+        if (this.state.nameFirst == '') {
+            bad = true;
+        }
+        
+        if (bad) {
+            this.state.badFirstName = true;
+        }
+        else {
+            this.state.badFirstName = false;
+        }
+        this.forceUpdate();
+        
+        if (bad)
+            return false;
+        return true;
+    }
+    
+    validateLastName() {
+        var bad = false;
+        if (this.state.nameLast == '') {
+            bad = true;
+        }
+        
+        if (bad) {
+            this.state.badLastName = true;
+        }
+        else {
+            this.state.badLastName = false;
+        }
+        this.forceUpdate();
+        
+        if (bad)
+            return false;
+        return true;
+    }
+    
+    validatePassword() {
+        var bad = false;
+        var misMatch = false;
+        if (this.state.password == '' || this.state.password.length < 8) {
+            bad = true;
+        }
+        if (this.state.password != this.state.password2 && this.state.loginState == 2)
+            misMatch = true;
+        
+        if (bad) {
+            this.state.badPassword = true;
+        }
+        else {
+            this.state.badPassword = false;
+        }
+        if (misMatch) {
+            this.state.badPasswordMatch = true;
+        }
+        else {
+            this.state.badPasswordMatch = false;
+        }
+        this.forceUpdate();
+        
+        if (bad || misMatch)
+            return false;
+        return true;
+    }
+    
+    
+    resetInvalids() {
+        this.state.badEmail = false;
+        this.state.badEmailEdu = false;
+        this.state.badFirstName = false;
+        this.state.badLastName = false;
+        this.state.badPassword = false;
+        this.state.badPasswordMatch = false;
+    }
  
+    
     // Function called once backend replies from login
     doneLogin() {
         let response = this.state.reply;
         console.log(response);
+        
+        var failed = false;
         if (response.code != null) {
             if (response.code == 3003) {
-                Alert.alert('Username or password was wrong, ya dingus');
-                return;
+                //Alert.alert('Username or password was wrong, ya dingus');
+                this.state.badPasswordMatch = true;
+                this.forceUpdate();
+                failed = true;
             }
-            Alert.alert('Login error');
-            return;
+            else {
+                Alert.alert('Login error');
+            }
+            failed = true;
         }
-        
+        if (failed)
+            return;
         
         // Code to enter actual app interface should go here
         this.state.loginState = 4;
@@ -68,16 +183,18 @@ export default class App extends Component {
     // Function called when login button pressed
     onLogin() {
         
+        this.resetInvalids();
+        
         const { loginState, email, firstName, lastName, password, password2 } = this.state;
         
-        if (email == '') {
-            Alert.alert('Please enter email');
-            return;
+        var failed = true;
+        
+        if (this.validateEmail() && this.validatePassword()) {
+            failed = false;
         }
-        if (password == '') {
-            Alert.alert('Please enter password');
+        
+        if (failed)
             return;
-        }
         
         
         fetch('https://api.backendless.com/C419EB67-D61C-013B-FF76-5EF8E90D6B00/E9273326-1DFD-8E37-FF7F-7EDDF2D19700/users/login', {
@@ -102,28 +219,23 @@ export default class App extends Component {
     
     // Function called when register button pressed
     onRegister() {
+        
+        this.resetInvalids();
+        
         const { loginState, email, nameFirst, nameLast, password, password2 } = this.state;
         
-        if (password != password2) {
-            Alert.alert('Passwords must match');
+        var failed = false;
+        
+        this.validateFirstName();
+        this.validateLastName();
+        this.validatePassword();
+        this.validateEmail();
+        
+        if (!this.validData())
+            failed = true;
+        
+        if (failed)
             return;
-        }
-        if (email == '') {
-            Alert.alert('Please enter email');
-            return;
-        }
-        if (password == '') {
-            Alert.alert('Please enter password');
-            return;
-        }
-        if (nameFirst == '') {
-            Alert.alert('Please enter your first name');
-            return;
-        }
-        if (nameLast == '') {
-            Alert.alert('Please enter your last name');
-            return;
-        }
         
         fetch('https://api.backendless.com/C419EB67-D61C-013B-FF76-5EF8E90D6B00/E9273326-1DFD-8E37-FF7F-7EDDF2D19700/data/Users', {
               method: 'POST',
@@ -148,6 +260,7 @@ export default class App extends Component {
     
     // Function called when dont have account button pressed
     switchRegister() {
+        this.resetInvalids();
         this.state.password = '';
         this.state.password2 = '';
         this.state.email = '';
@@ -159,22 +272,21 @@ export default class App extends Component {
     
     // Function called when already have account button pressed
     switchLogin() {
+        this.resetInvalids();
         this.state.loginState = 1;
         this.forceUpdate();
     }
     
     // Function called when logout button pressed
     logOut() {
-        this.setState({
-        loginState: 1,
-        email: '',
-        nameFirst: '',
-        nameLast: '',
-        password: '',
-        password2: '',
-        reply:[],
-        userToken: '',
-        });
+        this.resetInvalids();
+        this.state.email = '',
+        this.state.nameFirst = '',
+        this.state.nameLast = '',
+        this.state.password = '',
+        this.state.password2 = '',
+        this.state.userToken = '',
+        this.state.loginState = 1,
         this.forceUpdate();
     }
     
@@ -185,19 +297,28 @@ export default class App extends Component {
             case 1:     // Login Screen
                 return (
                     <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
-                    <TextInput
-                    value={this.state.email}
-                    onChangeText={(email) => this.setState({ email })}
-                    placeholder={'Email'}
-                    style={styles.input}
-                    />
-                    <TextInput
-                    value={this.state.password}
-                    onChangeText={(password) => this.setState({ password })}
-                    placeholder={'Password'}
-                    secureTextEntry={true}
-                    style={styles.input}
-                    />
+                    <View>
+                        <TextInput
+                        value={this.state.email}
+                        onChangeText={(email) => this.setState({ email })}
+                        onBlur = {() => this.validateEmail()}
+                        placeholder={'Email'}
+                        style={styles.input}
+                        />
+                        {this.state.badEmail ? <Text style={styles.errorMsg}>Invalid email</Text> : null}
+                    </View>
+                    <View>
+                        <TextInput
+                        value={this.state.password}
+                        onChangeText={(password) => this.setState({ password })}
+                        onBlur = {() => this.validatePassword()}
+                        placeholder={'Password'}
+                        secureTextEntry={true}
+                        style={styles.input}
+                        />
+                        {this.state.badPassword ? <Text style={styles.errorMsg}>Password cannot be blank</Text> : null}
+                        {this.state.badPasswordMatch ? <Text style={styles.errorMsg}>Username and password do not match </Text> : null}
+                    </View>
                     <Button
                     title={'Login'}
                     style={styles.input}
@@ -214,38 +335,59 @@ export default class App extends Component {
             case 2:     // Register Screen
                 return (
                     <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
-                    <TextInput
-                    value={this.state.nameFirst}
-                    onChangeText={(nameFirst) => this.setState({ nameFirst })}
-                    placeholder={'First Name'}
-                    style={styles.input}
-                    />
-                    <TextInput
-                    value={this.state.nameLast}
-                    onChangeText={(nameLast) => this.setState({ nameLast })}
-                    placeholder={'Last Name'}
-                    style={styles.input}
-                    />
-                    <TextInput
-                    value={this.state.email}
-                    onChangeText={(email) => this.setState({ email })}
-                    placeholder={'Email'}
-                    style={styles.input}
-                    />
-                    <TextInput
-                    value={this.state.password}
-                    onChangeText={(password) => this.setState({ password })}
-                    placeholder={'Password'}
-                    secureTextEntry={true}
-                    style={styles.input}
-                    />
-                    <TextInput
-                    value={this.state.password2}
-                    onChangeText={(password2) => this.setState({ password2 })}
-                    placeholder={'Confirm Password'}
-                    secureTextEntry={true}
-                    style={styles.input}
-                    />
+                    <View>
+                        <TextInput
+                        value={this.state.nameFirst}
+                        onChangeText={(nameFirst) => this.setState({ nameFirst })}
+                        onBlur = {() => this.validateFirstName()}
+                        placeholder={'First Name'}
+                        style={styles.input}
+                        />
+                        {this.state.badFirstName ? <Text style={styles.errorMsg}>First name cannot be blank</Text> : null}
+                    </View>
+                    <View>
+                        <TextInput
+                        value={this.state.nameLast}
+                        onChangeText={(nameLast) => this.setState({ nameLast })}
+                        onBlur = {() => this.validateLastName()}
+                        placeholder={'Last Name'}
+                        style={styles.input}
+                        />
+                        {this.state.badLastName ? <Text style={styles.errorMsg}>Last name cannot be blank</Text> : null}
+                    </View>
+                    <View>
+                        <TextInput
+                        value={this.state.email}
+                        onChangeText={(email) => this.setState({ email })}
+                        onBlur = {() => this.validateEmail()}
+                        placeholder={'Email'}
+                        style={styles.input}
+                        />
+                        {this.state.badEmail ? <Text style={styles.errorMsg}>Invalid email </Text> : null}
+                        {this.state.badEmailEdu ? <Text style={styles.errorMsg}>Must use .edu email</Text> : null}
+                    </View>
+                    <View>
+                        <TextInput
+                        value={this.state.password}
+                        onChangeText={(password) => this.setState({ password })}
+                        onBlur = {() => this.validatePassword()}
+                        placeholder={'Password'}
+                        secureTextEntry={true}
+                        style={styles.input}
+                        />
+                        {this.state.badPassword ? <Text style={styles.errorMsg}>Password must be 8 characters</Text> : null}
+                    </View>
+                    <View>
+                        <TextInput
+                        value={this.state.password2}
+                        onChangeText={(password2) => this.setState({ password2 })}
+                        onBlur = {() => this.validatePassword()}
+                        placeholder={'Confirm Password'}
+                        secureTextEntry={true}
+                        style={styles.input}
+                        />
+                        {this.state.badPasswordMatch ? <Text style={styles.errorMsg}>Passwords do not match</Text> : null}
+                    </View>
                     <Button
                     title={'Create Account'}
                     style={styles.input}
@@ -303,7 +445,15 @@ const styles = StyleSheet.create({
      height: 44,
      padding: 10,
      borderWidth: 1,
+     borderRadius: 5,
      borderColor: 'black',
      marginBottom: 10,
      },
+     errorMsg: {
+     color: 'red',
+     width: 200,
+     padding: 2,
+     borderWidth: 0,
+     marginBottom: 10,
+     }
      });
